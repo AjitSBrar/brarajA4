f162ac955fb92934681a99ee7f4deb5cbdd4103e
AjitSBrar <50676263+AjitSBrar@users.noreply.github.com>
2020-08-04 23:31:12 -0400

    Add files via upload

---
+++ assets/data/mydata.ts
@@ -0,0 +1,11 @@
+import { Braraj } from './../../app/braraj';
+
+
+export const MYINFO : Braraj = {
+    studID: '991538106',
+    studName:'Ajit Singh Brar',
+    studLoginName:'braraj',
+    studCampus:'Davis',
+    studTitle:'Assignment 4'
+
+}
